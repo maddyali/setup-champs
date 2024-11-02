08de006afd5deffded34f618d822c3f47810ba9b
Maddy Ali <maddyoali@gmail.com>
2024-11-02 11:32:32 -0500

    Add initial signup and login endpoints

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,3 +4,26 @@ exports.getSignup = (req, res) => {
 exports.getLogin = (req, res) => {
   res.render("login.ejs");
 };
+exports.postSignup = (req, res) => {
+  res.json({
+    timestamp: Date.now(),
+    msg: "signed up",
+    code: 200,
+  });
+};
+
+exports.postLogin = (req, res) => {
+  res.json({
+    timestamp: Date.now(),
+    msg: "login succesful",
+    code: 200,
+  });
+};
+
+exports.postLogout = (req, res) => {
+  res.json({
+    timestamp: Date.now(),
+    msg: "logout succesful",
+    code: 200,
+  });
+};
--- routes/main.js
+++ routes/main.js
@@ -7,4 +7,15 @@ router.get("/", homeController.getIndex);
 router.get("/signup", authController.getSignup);
 router.get("/login", authController.getLogin);
 
+// POST
+
+router.post("/signup", authController.postSignup);
+router.post("/login", authController.postLogin);
+router.post("/logout", authController.postLogout);
+
+// Catchall route for 404
+router.all("*", (req, res) => {
+  res.status(404).send("404 - Page Not Found");
+});
+
 module.exports = router;
