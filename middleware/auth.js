f1013a0d7499976912282cbfd2ef4d9b0c512d69
Maddy Ali <maddyoali@gmail.com>
2024-11-12 11:55:59 -0600

    Add route protection for /profile

---
+++ middleware/auth.js
@@ -0,0 +1,7 @@
+module.exports = function ensureAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/");
+  }
+};
--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const homeController = require("../controllers/home");
 const authController = require("../controllers/auth");
 const postsController = require("../controllers/posts");
+const ensureAuth = require("../middleware/auth");
 
 router.get("/", homeController.getIndex);
 router.get("/signup", authController.getSignup);
 router.get("/login", authController.getLogin);
 router.get("/logout", authController.logout);
-router.get("/profile", postsController.getProfile);
+router.get("/profile", ensureAuth, postsController.getProfile);
 
 // POST
 
