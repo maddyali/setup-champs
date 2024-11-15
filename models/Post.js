dc4ac4c429227e858609d8b4beae42566b79d775
Maddy Ali <maddyoali@gmail.com>
2024-11-15 11:35:21 -0600

    Add Post model

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  image: {
+    type: String,
+    // require: true,
+  },
+  caption: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
