25720ac2ea09fcad463f109ca0112be98ec36a75
Maddy Ali <maddyoali@gmail.com>
2024-12-23 12:42:03 -0600

    Add test users and make usernames unique

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, required: true },
+  userName: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: String,
 });
